56b0faf1ea50034a97f2de746c9c3fe928cb4ca4
Lucas Dimer Justo <37344488+LucasJusto@users.noreply.github.com>
2019-04-14 04:01:34 -0300

    father não será mais necessario

--- T1-Alest-II/Father.java
+++
@@ -1,9 +0,0 @@
-public class Father{
-    public String name, son;
-    public int childrenNumber;
-    public Father(String f,String s){
-        name=f;
-        son=s;
-        childrenNumber=1;
-    }
-}
